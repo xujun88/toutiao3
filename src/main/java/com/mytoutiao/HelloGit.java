c94dcbe11001565146d609e61e9cdd11abb4ada3
xujun <mitxujun@gmail.com>
2024-07-09 16:37:33 +0800

    first commit

---
+++ src/main/java/com/mytoutiao/HelloGit.java
@@ -0,0 +1,11 @@
+package com.mytoutiao;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello ignore");
+    }
+}
+
+
